c522864940a74b9d766aae3ac254e55785d36016
natalieursich <143512535+natalieursich@users.noreply.github.com>
2023-09-18 21:33:31 -0400

    homework - sept 18

--- Assignment 2/Debug.py
+++ Assignment 2/Debug.py
@@ -7,24 +7,29 @@ def main():
     name = input("your name is? ")
     print("Hello, ", name)
 
-    #this isn't printing anything :( so sad. what's wrong with her? Why she not printing out?
+
+    #this isn't printing anything :( so sad. what's wrong with her? Why she not printing out? --> need to include the print command in the next line
     color = input("what's your favorite color? ")
-    
-    #I thought i did this right :( can you fix it for me?
+    print(color, "is a cool color!")
+
+    #I thought i did this right :( can you fix it for me? --> add str()
     age = input("how old are you? ")
-    print("You are " + age + " years old")
-	
-    #Start with this one! We have a compilation error :(
+    print("You are " + str(age) + " years old")
+
+
+    #Start with this one! We have a compilation error :( --> add colon
     #Side note: you can put these statements in parentheses or not. it's up to you.
-    if (5 > 3)
+    if (5 > 3):
         print("5 is greater than 3")
-    
+
     #There are multiple correct answers and adjustments to this one 
     isFive = 5
     if isFive > 5:
-        print("isFive is greater than 5")
+        print(isFive, "is greater than 5")
+    elif isFive == 5:
+        print(isFive, "is equal to 5")
     else:
-        print("isFive is less than 5")
+        print(isFive, "is less than 5")
 
     #more multiple correct answers. Similar to the first. Adjust as perceived 
     toCheck = 4
@@ -32,20 +37,21 @@ def main():
         print(toCheck, "is greater than 5.")
     elif toCheck < 3:
 	    print(toCheck)
+    elif toCheck == 4:
+        print(toCheck, "is equal to 4")
     else:
         print(toCheck, "is 3")
 
     #Is it really printing the BEST option though? Rearrange these as you see fit
     toCheck = 5
-    if toCheck < 6:
-        print("toCheck is less than 6")
-    elif toCheck > 7:
+    if toCheck > 7:
         print("toCheck is greater than 7")
     elif toCheck == 5:
         print("toCheck is 5")
+    elif toCheck < 6:
+        print("toCheck is less than 6")
     else:
         print("toCheck is invalid")
-    
     
     
     
